e26feec4cd28077e104e784a3e4cfcdd10fe1771
Khaled Magdy <117130576+khaled-maggdy@users.noreply.github.com>
2025-01-12 17:06:46 +0200

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,147 @@
+export const Bio = {
+  name: "Khaled Magdy",
+  roles: [
+    "Backend Developer",
+  ],
+  description:
+    "I am a dedicated backend developer with expertise in PHP, MySQL, and Laravel. I am passionate about solving complex problems and building scalable web applications. Committed to growth, innovation, and delivering exceptional, user-focused results.",
+  github: "https://github.com/khaled-maggdy",
+  resume: "https://drive.google.com/file/d/11DT0W1a2wTcsLuW1wgGN5ZAmC1BKXPzz/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/khaledmaggdy",
+  twitter: "https://twitter.com/khaled_maggdy",
+  insta: "https://www.instagram.com/khaled_maggdy",
+  facebook: "https://www.facebook.com/khaled_maggdy",
+};
+
+export const skills = [
+
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "PHP",
+        image: "https://th.bing.com/th/id/R.ea54d3c2a1e2454ac1af435e8e0e321b?rik=ALuyg2V4My%2fIyg&riu=http%3a%2f%2flogonoid.com%2fimages%2fphp-logo.jpg&ehk=mj%2fGaeYPOFG5vjOCfP%2fUQxy7k3MQlDYZWndfJb5m2zc%3d&risl=&pid=ImgRaw&r=0",
+      },
+      {
+        name: "Laravel",
+        image:
+          "https://logospng.org/download/laravel/logo-laravel-icon-1024.png",
+      },
+     
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+     
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+     
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://youssefaboelkhir19.github.io/portofolio/newhorizions.png",
+    role: "Back-End Development",
+    company: "New Horizions",
+    date: "Jan 2024 - Oct 2024",
+    desc: "I successfully completed a Backend Development course in PHP and Laravel at New Horizons. The course focused on building and improving dynamic web applications using Laravel, a popular PHP framework. Key topics covered included Object-Oriented Programming (OOP), the MVC architecture, database management with MySQL, and designing RESTful APIs.",
+    skills: [
+      "PHP",
+      "LARAVEL",
+      "MYSQL",
+      "Object Orinted Programing (OOP)",
+      "MVC",
+    ],
+  }
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJrTsgR9816S64tWqn5kPctuzf54Jnp9B8Nw&s",
+    school: "Faculty science Departement SIM (software industry and multimedia)",
+    date: "Oct 2021",
+    desc: "student at Faculty of Science Alexandria University, Department of Software Industry and Multimedia (SIM),I joined the program on October 10, 2021, where I have been pursuing a specialized education in areas such as software development, multimedia technologies, and industry-focused applications. My studies combine both theoretical knowledge and practical skills, equipping me with the tools necessary to contribute to the rapidly evolving fields of technology and multimedia, with a focus on real-world applications in the software industry",
+    degree: "Bachelor of Technology - Computer Science",
+  },
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "LearnSphere",
+    date: "Jun 2024 ",
+    description:
+    "eveloped an online learning platform using Laravel",
+    image:
+      "https://github.com/rishavchanda/DecisionHub/raw/master/assets/testRule.jpg",
+    tags: [
+      "Laravel",
+      "MYSQL",
+      "PHP",
+    ],
+    category: "web app",
+    github: "https://github.com/khaled-maggdy/LearnSphere.git",
+    webapp: "https://decisionhub.netlify.app/",
+  },
+  {
+    id: 9,
+    title: "MovieStar",
+    date: "Oct 2024",
+    description:
+      "Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. \nAdmin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials:	#Email: testemployee@gmail.com	#Password- 123@Testemployee",
+    image:
+      "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
+    tags: [
+      "PHP",
+      "MYSQL",
+      "Laravel",
+      "React Js",
+      "Node Js",
+      "Express Js",
+      "Redux",
+    ],
+    category: "web app",
+    github: "https://github.com/mohamedalaskari/Movie-Star.git",
+    webapp: "https://trackify-management.netlify.app/",
+  },
+
+];
+
